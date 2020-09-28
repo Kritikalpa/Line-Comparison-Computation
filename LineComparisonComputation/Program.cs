a2c7d77932cc820493fd362de0a6e36f8acfaed3
Kriti Kalpa <krtklpsh@gmail.com>
2020-09-28 11:52:34 +0530

    [Kriti] Add. Length of a line calculator

--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace LineComparisonComputation
+namespace LineComparison
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation");
+            double[] x = new double[2];
+            double[] y = new double[2];
+            Random random = new Random();
+
+            for (int index = 0; index < 2; index++)
+            {
+                x[index] = random.Next(-100, 100);
+                y[index] = random.Next(-100, 100);
+            }
+            Console.WriteLine("Coordinates are ({0}, {1}) & ({2}, {3})", x[0], y[0], x[1], y[1]);
+
+            double length = Math.Sqrt(Math.Pow((x[1] - x[0]), 2) + Math.Pow((y[1] - y[0]), 2));
+            Console.WriteLine("Length of the line : " + Math.Round(length, 4));
         }
     }
 }
