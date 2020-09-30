0a23a4ea23182a94fb8052d1a3cab345db1506f3
Kriti Kalpa <krtklpsh@gmail.com>
2020-09-30 10:45:24 +0530

    [Kriti] Add. OOP Approach

--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -3,39 +3,74 @@ using System.Threading;
 
 namespace LineComparison
 {
-    class Program
+    public class Line
     {
-        static void Main(string[] args)
+        public double[] x = new double[2];
+        public double[] y = new double[2];
+        public int lineNum;
+        public double lineLength;
+
+        Random random = new Random();
+        public Line(int lineNum)
         {
-            double[] x = new double[2];
-            double[] y = new double[2];
-            double[] length = new double[2];
-            int lineCount = 0;
-
-            Random random = new Random();
-
-            while (lineCount < 2)
+            
+            this.lineNum = lineNum;
+            for (int index = 0; index < 2; index++)
             {
-                for (int index = 0; index < 2; index++)
-                {
-                    x[index] = random.Next(-100, 100);
-                    y[index] = random.Next(-100, 100);
-                }
-                Console.WriteLine("Coordinates of line {0} are ({1}, {2}) & ({3}, {4})", lineCount + 1, x[0], y[0], x[1], y[1]);
-
-                length[lineCount] = Math.Sqrt(Math.Pow((x[1] - x[0]), 2) + Math.Pow((y[1] - y[0]), 2));
-                Console.WriteLine("Length of the line {0}: {1}", lineCount + 1, Math.Round(length[lineCount], 4));
-
-                lineCount++;
+                this.x[index] = random.Next(-100, 100);
+                this.y[index] = random.Next(-100, 100);
             }
+        }
+        public void computeLineLength()
+        {
+            this.lineLength = Math.Sqrt(Math.Pow((x[1] - x[0]), 2) + Math.Pow((y[1] - y[0]), 2));
+        }
 
-            int value = length[0].CompareTo(length[1]);
+        public void toString()
+        {
+            Console.WriteLine("Coordinates of line {0} are ({1}, {2}) & ({3}, {4})", this.lineNum, x[0], y[0], x[1], y[1]);
+            Console.WriteLine("Length of Line {0} is {1} ", this.lineNum, Math.Round(this.lineLength, 4));
+        }
+    }
+
+    public class LineComparison 
+    { 
+        public void checkEquality(Line line1, Line line2)
+        {
+            if (line1.lineLength.Equals(line2.lineLength))
+                Console.WriteLine("Both lines are equal");
+            else
+                Console.WriteLine("Lines are not equal");
+        }
+
+        public void compareLines(Line line1, Line line2)
+        {
+            int value = line1.lineLength.CompareTo(line2.lineLength);
             if (value < 0)
                 Console.WriteLine("Line 1 is smaller than Line 2");
             else if (value == 0)
                 Console.WriteLine("Both the lines are equal");
             else
                 Console.WriteLine("Line 1 is greater than Line 2");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Line line1 = new Line(1);
+            Line line2 = new Line(2);
+
+            line1.computeLineLength();
+            line2.computeLineLength();
+
+            line1.toString();
+            line2.toString();
+
+            LineComparison lineToLineCompare = new LineComparison();
+            lineToLineCompare.checkEquality(line1, line2);
+            lineToLineCompare.compareLines(line1, line2);
+            
         }
     }
 }
